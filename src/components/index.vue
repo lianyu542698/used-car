65028aa112f82b6aea22fac0837b794cbc502377
lianyu <974691151@qq.com>
2018-12-03 17:50:31 +0800

    .

---
+++ src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <p>我是首页</p>
+  </div>
+</template>
+
+<script>
+import http from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  created () {
+    http.get('')
+    .then((res)=>{
+      console.log(res)
+      }
+    )
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'lib-flexible'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
